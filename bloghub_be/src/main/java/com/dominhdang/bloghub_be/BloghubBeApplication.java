e5db606c12de1954aeb86f83322b65cd3f6a8cb9
Dang Do Minh <dangdm3@fpt.com>
2024-10-18 19:36:11 +0700

    inti project

---
+++ bloghub_be/src/main/java/com/dominhdang/bloghub_be/BloghubBeApplication.java
@@ -0,0 +1,13 @@
+package com.dominhdang.bloghub_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloghubBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloghubBeApplication.class, args);
+	}
+
+}
---
+++ bloghub_be/src/test/java/com/dominhdang/bloghub_be/BloghubBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dominhdang.bloghub_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloghubBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
