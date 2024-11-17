53935e9486fefa375fbeacc1692e15fe29bf3372
dominhdang148 <myclone235@gmail.com>
2024-11-17 16:53:50 +0700

    #6 Finished Header with Responsive

--- bloghub_fe/src/components/Header/Header.tsx
+++ bloghub_fe/src/components/Header/Header.tsx
@@ -27,46 +27,68 @@ const Header = () => {
                         />
                         <strong>NovelHub</strong>
                     </Link>
+                    <button
+                        className="navbar-toggler"
+                        data-bs-toggle="offcanvas"
+                        data-bs-target="#menu-item"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
                     {/* Menu Item */}
-                    <div className="collapse navbar-collapse">
-                        <ul className="nav navbar-nav">
-                            <li className="nav-item">
-                                <Link
-                                    className="nav-link d-flex gap-1 align-items-center"
-                                    href={'#'}
-                                >
-                                    <FontAwesomeIcon icon={faSearch} />
-                                    <span>Search</span>
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link
-                                    className="nav-link d-flex gap-1 align-items-center"
-                                    href={'#'}
-                                >
-                                    <FontAwesomeIcon icon={faList} />
-                                    <span>Categorys</span>
-                                </Link>
-                            </li>
-                            <li className="nav-item ">
-                                <Link
-                                    className="nav-link d-flex gap-1 align-items-center"
-                                    href={'#'}
-                                >
-                                    <FontAwesomeIcon icon={faTags} />
-                                    <span>Tags</span>
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link
-                                    className="nav-link d-flex gap-1 align-items-center"
-                                    href={'#'}
-                                >
-                                    <FontAwesomeIcon icon={faFlagCheckered} />
-                                    <span>Updates</span>
-                                </Link>
-                            </li>
-                        </ul>
+                    <div
+                        className="offcanvas offcanvas-end container-fluid"
+                        tabIndex={-1}
+                        id="menu-item"
+                    >
+                        <div className="offcanvas-header">
+                            <button
+                                type="button"
+                                className="btn-close"
+                                data-bs-dismiss="offcanvas"
+                            ></button>
+                        </div>
+                        <div className="offcanvas-body">
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <Link
+                                        className="nav-link d-flex gap-1 align-items-center"
+                                        href={'#'}
+                                    >
+                                        <FontAwesomeIcon icon={faSearch} />
+                                        <span>Search</span>
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link
+                                        className="nav-link d-flex gap-1 align-items-center"
+                                        href={'#'}
+                                    >
+                                        <FontAwesomeIcon icon={faList} />
+                                        <span>Categorys</span>
+                                    </Link>
+                                </li>
+                                <li className="nav-item ">
+                                    <Link
+                                        className="nav-link d-flex gap-1 align-items-center"
+                                        href={'#'}
+                                    >
+                                        <FontAwesomeIcon icon={faTags} />
+                                        <span>Tags</span>
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link
+                                        className="nav-link d-flex gap-1 align-items-center"
+                                        href={'#'}
+                                    >
+                                        <FontAwesomeIcon
+                                            icon={faFlagCheckered}
+                                        />
+                                        <span>Updates</span>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                     {/* Loginbutton */}
                 </div>
