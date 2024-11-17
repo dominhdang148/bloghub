35575ae888a10782ef36b38bb580ed530d67dc0f
Đỗ Minh Đăng <myclone235@gmail.com>
2024-11-17 17:04:04 +0700

    #6 - Header Ready to merge (#13)
    * add taiwind successfully
    
    * refactor to bootstrap
    
    * In progress
    
    * finished large-size navbar
    
    * #6 Finished Header with Responsive

--- bloghub_fe/src/app/page.tsx
+++ bloghub_fe/src/app/page.tsx
@@ -1,7 +1,5 @@
-export default function Home() {
-    return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
-    )
+import Home from './home/page';
+
+export default function Root() {
+    return <Home />;
 }
---
+++ bloghub_fe/src/components/Header/Header.tsx
@@ -0,0 +1,100 @@
+import {
+    faBookJournalWhills,
+    faFlagCheckered,
+    faList,
+    faSearch,
+    faTags,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
+
+const Header = () => {
+    return (
+        <header>
+            <nav
+                className="navbar navbar-expand-lg bg-dark"
+                data-bs-theme="dark"
+            >
+                <div className="container-fluid">
+                    {/* Logo */}
+                    <Link
+                        href={'/'}
+                        className="navbar-brand d-flex align-items-center gap-3"
+                    >
+                        <FontAwesomeIcon
+                            icon={faBookJournalWhills}
+                            className="border border-1 rounded-4 p-3 border-light"
+                        />
+                        <strong>NovelHub</strong>
+                    </Link>
+                    <button
+                        className="navbar-toggler"
+                        data-bs-toggle="offcanvas"
+                        data-bs-target="#menu-item"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    {/* Menu Item */}
+                    <div
+                        className="offcanvas offcanvas-end container-fluid"
+                        tabIndex={-1}
+                        id="menu-item"
+                    >
+                        <div className="offcanvas-header">
+                            <button
+                                type="button"
+                                className="btn-close"
+                                data-bs-dismiss="offcanvas"
+                            ></button>
+                        </div>
+                        <div className="offcanvas-body">
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <Link
+                                        className="nav-link d-flex gap-1 align-items-center"
+                                        href={'#'}
+                                    >
+                                        <FontAwesomeIcon icon={faSearch} />
+                                        <span>Search</span>
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link
+                                        className="nav-link d-flex gap-1 align-items-center"
+                                        href={'#'}
+                                    >
+                                        <FontAwesomeIcon icon={faList} />
+                                        <span>Categorys</span>
+                                    </Link>
+                                </li>
+                                <li className="nav-item ">
+                                    <Link
+                                        className="nav-link d-flex gap-1 align-items-center"
+                                        href={'#'}
+                                    >
+                                        <FontAwesomeIcon icon={faTags} />
+                                        <span>Tags</span>
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link
+                                        className="nav-link d-flex gap-1 align-items-center"
+                                        href={'#'}
+                                    >
+                                        <FontAwesomeIcon
+                                            icon={faFlagCheckered}
+                                        />
+                                        <span>Updates</span>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    {/* Loginbutton */}
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
