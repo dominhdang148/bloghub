abc278235e6d8841b68c1b61c80022535962bf63
dominhdang148 <myclone235@gmail.com>
2024-11-17 16:15:04 +0700

    finished large-size navbar

--- bloghub_fe/src/app/layout.tsx
+++ bloghub_fe/src/app/layout.tsx
@@ -1,8 +1,11 @@
 import BootstrapLoader from '@/components/BootstrapLoader/BootstrapLoader';
 import Header from '@/components/Header/Header';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './globals.css';
 
+config.autoAddCss = false;
 const RootLayout: React.FC<Readonly<{ children: React.ReactNode }>> = ({
     children,
 }) => {
--- bloghub_fe/src/components/Header/Header.tsx
+++ bloghub_fe/src/components/Header/Header.tsx
@@ -1,3 +1,11 @@
+import {
+    faBookJournalWhills,
+    faFlagCheckered,
+    faList,
+    faSearch,
+    faTags,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
 
 const Header = () => {
@@ -9,11 +17,56 @@ const Header = () => {
             >
                 <div className="container-fluid">
                     {/* Logo */}
-                    <Link href={'/'} className="navbar-brand">
-                        NovelHub
+                    <Link
+                        href={'/'}
+                        className="navbar-brand d-flex align-items-center gap-3"
+                    >
+                        <FontAwesomeIcon
+                            icon={faBookJournalWhills}
+                            className="border border-1 rounded-4 p-3 border-light"
+                        />
+                        <strong>NovelHub</strong>
                     </Link>
                     {/* Menu Item */}
-                    <div className="nav">
+                    <div className="collapse navbar-collapse">
+                        <ul className="nav navbar-nav">
+                            <li className="nav-item">
+                                <Link
+                                    className="nav-link d-flex gap-1 align-items-center"
+                                    href={'#'}
+                                >
+                                    <FontAwesomeIcon icon={faSearch} />
+                                    <span>Search</span>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link
+                                    className="nav-link d-flex gap-1 align-items-center"
+                                    href={'#'}
+                                >
+                                    <FontAwesomeIcon icon={faList} />
+                                    <span>Categorys</span>
+                                </Link>
+                            </li>
+                            <li className="nav-item ">
+                                <Link
+                                    className="nav-link d-flex gap-1 align-items-center"
+                                    href={'#'}
+                                >
+                                    <FontAwesomeIcon icon={faTags} />
+                                    <span>Tags</span>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link
+                                    className="nav-link d-flex gap-1 align-items-center"
+                                    href={'#'}
+                                >
+                                    <FontAwesomeIcon icon={faFlagCheckered} />
+                                    <span>Updates</span>
+                                </Link>
+                            </li>
+                        </ul>
                     </div>
                     {/* Loginbutton */}
                 </div>
